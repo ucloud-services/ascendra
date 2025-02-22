5234ccdbac28489b225bf278368a1971e6b28474
Roberto Ungarelli <roberto.ungarelli@ucloud.services>
2025-02-22 14:36:41 +0000

    Feat/rbt (#1)
    * iac(feat/rbt): creating base resources
    
    * iac(feat/rbt): creating base resources

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.AWS_AKA
+  secret_key = var.AWS_SKA
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "AWS_AKA" {
+    type = string
+    description = "AWS access key"
+}
+
+variable "AWS_SKA" {
+    type = string
+    description = "AWS secret key"
+}
